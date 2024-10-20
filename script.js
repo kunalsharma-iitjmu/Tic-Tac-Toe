0a3d564d29305a0a195bb67a10509be7cd269aa4
Kunal Sharma <159030119+kunalsharma-iitjmu@users.noreply.github.com>
2024-10-20 19:10:27 +0530

    ok

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ let box = document.querySelectorAll(".boxes");
 let cont = document.querySelector(".container");
 let msgContainer = document.querySelector(".winner");
 let msg = document.querySelector("#msg");
+let congo = document.querySelector("#congo");
 
 let turn0 = true;   //To Identify player O or player X
+let winner = false;
+let count = 0;
 
 const wincases = [
     [0, 1, 2],
@@ -31,13 +34,25 @@ box.forEach((bx) => {
             turn0 = true;
         }
         bx.disabled = true;
-
+        count++;
         checkWinner();
+
+        if(count === 9 && winner === false){
+            gameDraw();
+        }
     });
 });
 
+const gameDraw = () => {
+    msg.innerText = `Game was a Draw.`;
+    msgContainer.classList.remove("hide");
+    cont.classList.add("hide");
+    congo.classList.add("hide");
+    disableBoxes();
+};
+
 const disableBoxes = () => {
-    for( let b of box){
+    for (let b of box) {
         b.disabled = true;
     }
 }
@@ -51,15 +66,16 @@ const showWinner = (win) => {
 
 const checkWinner = () => {
     for (let pattern of wincases) {
-        let pos1val = box[pattern[0]].innerText;   
+        let pos1val = box[pattern[0]].innerText;
         let pos2val = box[pattern[1]].innerText;
         let pos3val = box[pattern[2]].innerText;
 
-        if(pos1val != "" && pos2val != "" && pos3val != ""){
-            if(pos1val === pos2val && pos2val === pos3val){
-                console.log("Winner" , pos1val);
+        if (pos1val != "" && pos2val != "" && pos3val != "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                console.log("Winner", pos1val);
+                winner = true;
                 showWinner(pos1val);
             };
-        };
+        }
     };
 };
